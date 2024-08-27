61226a87c73dfd91aba6f8ffc34a728200359b01
eyakub175 <eyakub.bubt@gmail.com>
2024-08-28 00:29:21 +0600

    Create introString.js

---
+++ Strings/introString.js
@@ -0,0 +1,28 @@
+let text1 = "It's me";
+let text2 =  "I'm not your plz!";
+let text3 = 'I love only "Dona" ';
+
+//template Strings
+
+let forHer = `I'm your love "Dona"`;
+
+let length = forHer.length;
+
+console.log(length);
+
+
+//Escape Characters
+
+let me = "I'm student of \"BUBT\" in mirpur ";
+
+let you = 'It\'s ok!';
+console.log(you);
+
+
+let x = "Eyakub";
+let y = "175";
+
+console.log(x===y);
+console.log(typeof y);
+
+
