81508d94718065bdc760c9ddba7e5636c44d2e9c
eyakub175 <eyakub.bubt@gmail.com>
2024-08-27 21:32:47 +0600

    Create let.js

---
+++ variables/let.js
@@ -0,0 +1,18 @@
+// Variables declared with let have Block Scope
+
+// Variables declared with let must be Declared before use
+
+// Variables declared with let cannot be Redeclared in the same scope
+
+
+//declare a variable
+ let x;
+
+ // assign variable
+
+ x = 3 ;
+
+ //output 
+
+ console.log(x);
+ 
