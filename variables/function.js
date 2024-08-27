ce7e167896c58b3879e58a97fc1bd79046e461c9
eyakub175 <eyakub.bubt@gmail.com>
2024-08-27 22:08:11 +0600

    All variable in variables

---
+++ variables/function.js
@@ -0,0 +1,12 @@
+/* function name(parameter1, parameter2, parameter3) {
+     code to be executed
+  } */
+
+
+let x = myFunction(3,4);
+
+function myFunction(a, b){
+    return a * b;
+}
+
+console.log(x);
---
+++ variables/objects.js
@@ -0,0 +1,20 @@
+//declare and assign a object...
+
+const person = {name:"Eyakub", dept:"CSE", id: "175"};
+
+//Create another object
+
+const person1 = {};
+
+//add properties
+
+person1.firstName = "Eyakub";
+person1.lastName = "Ali";
+person.age = "25";
+
+
+/* Methods are actions that can be performed on objects.
+
+Methods are function definitions stored as property values. */
+
+
